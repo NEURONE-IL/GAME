295fde780c36f9488750072d333d5d01d6f25150
Cristobal <cristobal.tbecerraf@gmail.com>
2021-03-27 09:41:50 -0300

    validate study before play

--- Client/src/app/views/user-profile/user-profile.component.ts
+++ Client/src/app/views/user-profile/user-profile.component.ts
@@ -4,132 +4,181 @@ import { Router } from '@angular/router';
 import { ImageSelectorComponent } from 'src/app/components/image-selector/image-selector.component';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { GamificationService } from 'src/app/services/game/gamification.service';
+import { StudyService } from 'src/app/services/game/study.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent implements OnInit {
-
-  progress
-  nearLevel
-  completedChallenges
-  actualLevel
-  points
-  user
-  ranks
-  constructor(private gamificationService: GamificationService,  private authService: AuthService, public dialog: MatDialog, public router: Router) { }
+  progress;
+  nearLevel;
+  completedChallenges;
+  actualLevel;
+  points;
+  user;
+  ranks;
+  studyOk = false;
+  constructor(
+    private gamificationService: GamificationService,
+    private authService: AuthService,
+    public dialog: MatDialog,
+    public router: Router,
+    private studyService: StudyService
+  ) {}
 
   ngOnInit(): void {
     this.user = this.authService.getUser();
+    this.checkStudy();
     this.status();
-
   }
 
-  getCompletedChallenges(){
-    this.gamificationService.userCompletedChallenges(this.authService.getUser()._id).subscribe(
-      response => {
-        this.completedChallenges = response;
-        for(let i = 0; i<this.completedChallenges.length; i++){
-          this.completedChallenges[i].dateDisplay = new Date(this.completedChallenges[i].completion_date).toLocaleDateString();
-        }
-      },
-      err => {
-        console.log(err)
-      }
-    );
-  }
-
-  getLevels(){
-    this.gamificationService.userLevel(this.authService.getUser()._id).subscribe(
-      response => {
-        let levels = response;
-        this.actualLevel = levels[0];
-        for(let i = 0; i<levels.length; i++){
-          if(levels[i].point_threshold < this.actualLevel.point_threshold){
-            this.actualLevel = levels[i];
+  getCompletedChallenges() {
+    this.gamificationService
+      .userCompletedChallenges(this.authService.getUser()._id)
+      .subscribe(
+        (response) => {
+          this.completedChallenges = response;
+          for (let i = 0; i < this.completedChallenges.length; i++) {
+            this.completedChallenges[i].dateDisplay = new Date(
+              this.completedChallenges[i].completion_date
+            ).toLocaleDateString();
           }
+        },
+        (err) => {
+          console.log(err);
         }
-      },
-      err => {
-        console.log(err)
-      }
-    );
+      );
   }
 
-  getPoints(){
-    this.gamificationService.userPoints(this.authService.getUser()._id).subscribe(
-      response => {
-        this.points = response;
-      },
-      err => {
-        console.log(err)
-      }
-    );
-  }
-
-  levelProgress(){
-    this.gamificationService.userLevelProgress(this.authService.getUser()._id).subscribe(
-      response => {
-        this.progress = response;
-        this.nearLevel = this.progress[0];
-        for(let i = 1; i<this.progress.length; i++){
-          if(this.progress[i].point_threshold < this.nearLevel.point_threshold){
-            this.nearLevel = this.progress[i];
+  getLevels() {
+    this.gamificationService
+      .userLevel(this.authService.getUser()._id)
+      .subscribe(
+        (response) => {
+          let levels = response;
+          this.actualLevel = levels[0];
+          for (let i = 0; i < levels.length; i++) {
+            if (levels[i].point_threshold < this.actualLevel.point_threshold) {
+              this.actualLevel = levels[i];
+            }
           }
+        },
+        (err) => {
+          console.log(err);
         }
-        document.getElementById("progressLevel").setAttribute("data-label", this.nearLevel.amount + '/' + this.nearLevel.point_threshold+' '+ this.nearLevel.point.name);
-        document.getElementById("progressValue").style.width = this.nearLevel.amount/this.nearLevel.point_threshold*100+'%'
+      );
+  }
 
-      },
-      err => {
-        console.log(err)
-      }
-    );
+  getPoints() {
+    this.gamificationService
+      .userPoints(this.authService.getUser()._id)
+      .subscribe(
+        (response) => {
+          this.points = response;
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
+
+  levelProgress() {
+    this.gamificationService
+      .userLevelProgress(this.authService.getUser()._id)
+      .subscribe(
+        (response) => {
+          this.progress = response;
+          this.nearLevel = this.progress[0];
+          for (let i = 1; i < this.progress.length; i++) {
+            if (
+              this.progress[i].point_threshold < this.nearLevel.point_threshold
+            ) {
+              this.nearLevel = this.progress[i];
+            }
+          }
+          document
+            .getElementById('progressLevel')
+            .setAttribute(
+              'data-label',
+              this.nearLevel.amount +
+                '/' +
+                this.nearLevel.point_threshold +
+                ' ' +
+                this.nearLevel.point.name
+            );
+          document.getElementById('progressValue').style.width =
+            (this.nearLevel.amount / this.nearLevel.point_threshold) * 100 +
+            '%';
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
   }
   openImageSelector() {
     const dialogRef = this.dialog.open(ImageSelectorComponent);
-    dialogRef.afterClosed().subscribe(res => {
-      this.gamificationService.changeProfileImage(this.authService.getUser()._id, res).subscribe(response => {
-        this.authService.refreshUser();
-        this.user = this.authService.getUser();
-      })
-      console.log(res)
+    dialogRef.afterClosed().subscribe((res) => {
+      this.gamificationService
+        .changeProfileImage(this.authService.getUser()._id, res)
+        .subscribe((response) => {
+          this.authService.refreshUser();
+          this.user = this.authService.getUser();
+        });
+      console.log(res);
     });
   }
-  rankings(){
-    this.gamificationService.userRankings(this.authService.getUser()._id, 'ranking_exp').subscribe(
-      response => {
-        let ranks = [];
-        for(let i = 0; i<response.leaderboardResult.length; i++){
-          if(response.leaderboardResult[i].code === this.user.gm_code){
-            ranks.push(response.leaderboardResult[i].rank.toString()+'° en cantidad de Experiencia')
+  rankings() {
+    this.gamificationService
+      .userRankings(this.authService.getUser()._id, 'ranking_exp')
+      .subscribe(
+        (response) => {
+          let ranks = [];
+          for (let i = 0; i < response.leaderboardResult.length; i++) {
+            if (response.leaderboardResult[i].code === this.user.gm_code) {
+              ranks.push(
+                response.leaderboardResult[i].rank.toString() +
+                  '° en cantidad de Experiencia'
+              );
+            }
           }
+          this.ranks = ranks;
+        },
+        (err) => {
+          console.log(err);
         }
-        this.ranks = ranks;
-
-      },
-      err => {
-        console.log(err)
-      }
-    );
+      );
   }
 
-  status(){
-    this.gamificationService.gamificationStatus().subscribe(response => {
-      if(response.gamified){
+  status() {
+    this.gamificationService.gamificationStatus().subscribe((response) => {
+      if (response.gamified) {
         this.levelProgress();
         this.getCompletedChallenges();
         this.getLevels();
         this.getPoints();
         this.rankings();
       }
-    })
+    });
   }
 
-  play(){
+  checkStudy() {
+    let studyId = this.user.study;
+    this.studyService.getStudy(studyId).subscribe((res) => {
+      if(res.study) {
+        this.studyOk = true;
+      }
+      else {
+        console.log("Study assigned to this user doesn't exist");
+      }
+    },
+    (err) => {
+      console.log(err);
+    });
+  }
+
+  play() {
     this.router.navigateByUrl('/start');
   }
-
 }
