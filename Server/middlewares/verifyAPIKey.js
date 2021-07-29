ed575a0b6fde454958fb8625610aad1e810ff280
Jose <jose.mellado@usach.cl>
2021-07-29 02:06:59 -0400

    api key

--- Server/middlewares/verifyAPIKey.js
+++ Server/middlewares/verifyAPIKey.js
@@ -5,8 +5,10 @@ module.exports = function auth(req, res, next){
     const token = req.header('x-api-key');
     //checking token is provided    
     if(!token) return res.status(401).send('No key provided!');
+    console.log(req.headers.origin);
     const host = req.headers.origin;
     Site.findOne({host: host}, (err, site) => {
+        console.log(site);
         if(err || !site){
             res.status(401).send('Not authorized');
         }
