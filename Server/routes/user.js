49f5bfc0efd9418fd8a97ed2f5b6281e30eeb13a
Jose <jose.mellado@usach.cl>
2021-08-15 14:26:20 -0400

    fix progress error

--- Server/routes/user.js
+++ Server/routes/user.js
@@ -179,14 +179,14 @@ router.put("/:user_id/profileImage", [verifyToken], async (req, res) => {
 
 router.get("/:user_id/progress", [verifyToken], async (req, res) => {
   const userId = req.params.user_id;
-  const user = await User.findOne({ _id: _id }, err => {
+  const user = await User.findOne({ _id: userId }, err => {
     if (err) {
       return res.status(404).json({
         err,
       });
     }
   });
-  UserStudy.findOne({ user: userId, study: user.study }, async (err, userStudy) => {
+  UserStudy.findOne({ user: user._id, study: user.study }, async (err, userStudy) => {
     if (err) {
       return res.status(404).json({
         ok: false,
