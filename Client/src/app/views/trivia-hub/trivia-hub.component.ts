95a3595826b6acecb04e2ac36c393bc63b6bf410
Fernando Villarreal <fernando.villarreal@usach.cl>
2021-09-27 20:04:35 -0300

    Fix: Welcome view 'Participar' button bug fixed. Minor fixes.

--- Client/src/app/views/trivia-hub/trivia-hub.component.ts
+++ Client/src/app/views/trivia-hub/trivia-hub.component.ts
@@ -7,7 +7,7 @@ import { Study, StudyService } from 'src/app/services/game/study.service';
 @Component({
   selector: 'app-trivia-hub',
   templateUrl: './index.html',
-  styleUrls: ['./css/style.css']
+  styleUrls: ['./css/style.min.css']
 })
 
 /*export class TriviaHubComponent implements OnInit {
@@ -40,6 +40,24 @@ export class TriviaHubComponent implements OnInit {
              ) { }
 
   ngOnInit(): void {
+    if(localStorage.getItem('study_id')){
+      let study = localStorage.getItem('study_id');
+      this.studyService.getStudy(study).subscribe(
+        response => {
+          if(response['study']){
+            this.study = response['study'];
+            this.containsStudy = true;
+            localStorage.setItem('study_id', this.study._id);
+          };
+        },
+        err => {
+          this.toastr.error(this.translate.instant("STUDY.TOAST.WELCOME_ERROR"), this.translate.instant("STUDY.TOAST.ERROR"), {
+            timeOut: 5000,
+            positionClass: 'toast-top-center'
+          });
+        }
+      );
+    }
     if(this.router.url.toString().split("/").length > 2){
       let urlArray = this.router.url.toString().split("/");
       let study = urlArray[urlArray.length - 1];
@@ -48,6 +66,7 @@ export class TriviaHubComponent implements OnInit {
           if(response['study']){
             this.study = response['study'];
             this.containsStudy = true;
+            localStorage.setItem('study_id', this.study._id);
           };
         },
         err => {
@@ -61,7 +80,7 @@ export class TriviaHubComponent implements OnInit {
   }
 
   getStudy(){
-    return '/signup/' + this.study._id;
+    return '/signup/' + localStorage.getItem('study_id');
   }
 
 }
