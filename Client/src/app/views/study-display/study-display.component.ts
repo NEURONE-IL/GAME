7a7c645cff0d1fbe022ff1b0f0eba17680d3aaa9
Jose <jose.mellado@usach.cl>
2021-10-25 19:56:38 -0300

    incidencias jose

--- Client/src/app/views/study-display/study-display.component.ts
+++ Client/src/app/views/study-display/study-display.component.ts
@@ -329,7 +329,7 @@ export class StudyDisplayComponent implements OnInit {
 export class StudyUpdateDialogComponent implements OnInit{
   studyForm: FormGroup;
   hours: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
-  maxPers: number[] = [1, 2, 3, 4, 5];
+  maxPers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
   minutes: number[] = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
   loading: Boolean;
   file: File;
